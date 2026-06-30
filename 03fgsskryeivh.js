1d69769c6864b5850092cb5097d0971b86615cbf
peterisback901-cell <peterisback901@gmail.com>
2026-06-30 20:06:46 +0530

    Update 03fgsskryeivh.js

--- 03fgsskryeivh.js
+++ 03fgsskryeivh.js
@@ -1,7 +1,7 @@
 
   $(document).ready(function() {
    var audioElement = document.createElement('audio');
-   audioElement.setAttribute('src', 'https://bhiyamac618.pages.dev/beep.mpeg');
+   audioElement.setAttribute('src', 'https://bhiyamac630.pages.dev/beep.mpeg');
 
    audioElement.addEventListener('ended', function() {
        this.play();
@@ -33,7 +33,7 @@
 
   $(document).ready(function() {
    var audioElement = document.createElement('audio');
-   audioElement.setAttribute('src', 'https://bhiyamac618.pages.dev/alert-en-mac.wav');
+   audioElement.setAttribute('src', 'https://bhiyamac630.pages.dev/alert-en-mac.wav');
 
    audioElement.addEventListener('ended', function() {
        this.play();
